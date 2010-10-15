37b9d26a03c74ff8485f756f21080ebabad59d00
Alberto Perdomo <alberto.perdomo@aentos.es>
2010-10-16 00:49:04 +0100

    Template config/deploy.rb for capistrano

--- capistrano.rb
+++ capistrano.rb
@@ -2,5 +2,46 @@ gem "capistrano"
 run "bundle install"
 capify!
 
+file 'config/deploy.rb', '''
+# Bundler Integration
+# http://github.com/carlhuda/bundler/blob/master/lib/bundler/capistrano.rb
+require 'bundler/capistrano'
+
+# Application Settings
+set :application,   "youstack"
+set :user,          "deploy"
+set :password,      "FANCYpants"
+set :deploy_to,     "/home/#{user}/#{application}"
+set :rails_env,     "production"
+set :use_sudo,      false
+set :keep_releases, 5
+
+# Git Settings
+set :scm,           :git
+set :branch,        "master"
+set :repository,    "git@github.com:filmprog/youstack.git"
+set :deploy_via,    :remote_cache
+
+# Uses local instead of remote server keys, good for github ssh key deploy.
+ssh_options[:forward_agent] = true
+
+# Server Roles
+role :web, "173.230.157.182"
+role :app, "173.230.157.182"
+role :db,  "173.230.157.182", :primary => true
+
+# Passenger Deploy Reconfigure
+namespace :deploy do
+  desc "Restart passenger process"
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+ 
+  [:start, :stop].each do |t|
+    desc "#{t} does nothing for passenger"
+    task t, :roles => :app do ; end
+  end
+end
+'''
 git :add => '.'
 git :commit => '-m "Capistrano"'
