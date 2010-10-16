49c7154d2d341d06e8dd8b334934cddfbfc42afb
Alberto Perdomo <alberto.perdomo@aentos.es>
2010-10-16 01:02:51 +0100

    Capistrano

--- capistrano.rb
+++ capistrano.rb
@@ -2,16 +2,20 @@ gem "capistrano"
 run "bundle install"
 capify!
 
-file 'config/deploy.rb', '''
+app_name = ask "\r\n\r\nEnter the application name:"
+server = ask "\r\n\r\nEnter the servername or IP:"
+git_repo = ask "\r\n\r\nEnter the git repo URL, e.g. git@github.com:aentos/app.git :"
+
+file 'config/deploy.rb', <<-FILE
 # Bundler Integration
 # http://github.com/carlhuda/bundler/blob/master/lib/bundler/capistrano.rb
 require 'bundler/capistrano'
 
 # Application Settings
-set :application,   "youstack"
-set :user,          "deploy"
-set :password,      "FANCYpants"
-set :deploy_to,     "/home/#{user}/#{application}"
+set :application,   "#{app_name}"
+set :user,          "deployer"
+set :password,      "deployer"
+set :deploy_to,     "/var/rails/#{app_name}"
 set :rails_env,     "production"
 set :use_sudo,      false
 set :keep_releases, 5
@@ -19,29 +23,29 @@ set :keep_releases, 5
 # Git Settings
 set :scm,           :git
 set :branch,        "master"
-set :repository,    "git@github.com:filmprog/youstack.git"
+set :repository,    "#{git_repo}"
 set :deploy_via,    :remote_cache
 
 # Uses local instead of remote server keys, good for github ssh key deploy.
 ssh_options[:forward_agent] = true
 
 # Server Roles
-role :web, "173.230.157.182"
-role :app, "173.230.157.182"
-role :db,  "173.230.157.182", :primary => true
+role :web, "#{server}"
+role :app, "#{server}"
+role :db,  "#{server}", :primary => true
 
 # Passenger Deploy Reconfigure
 namespace :deploy do
   desc "Restart passenger process"
   task :restart, :roles => :app, :except => { :no_release => true } do
-    run "touch #{current_path}/tmp/restart.txt"
+    run "touch \#{current_path}/tmp/restart.txt"
   end
  
   [:start, :stop].each do |t|
-    desc "#{t} does nothing for passenger"
+    desc "\#{t} does nothing for passenger"
     task t, :roles => :app do ; end
   end
 end
-'''
+FILE
 git :add => '.'
 git :commit => '-m "Capistrano"'
